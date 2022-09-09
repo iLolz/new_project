61837cd4cd84bf8451f5923e9d366b27e3507c42
Amanda Mendanha <amandinhamendanha74@gmail.com>
2022-09-08 22:30:21 -0300

    Adicionar Amanda

--- lib/new_project.dart
+++ lib/new_project.dart
@@ -1,16 +1,5 @@
 int calculate() {
-  print("Alessandro Bento da Silva");
   print("Alencar A. Moreira");
-  print("Paula F. Rodrigues");
-  print("Vanessa de Oliveira Mello");
-  print("Fabio Martinelli");
-  print("Hugo Pedro Daudt Jr");
-  print("Mateus C. Rossi");
-  print("Evandro Pedro Alves de Mendonça.");
-  print("Rodrigo A. OLiveira");
-  print("Fábio Wood");
-  print("Leandro Oliveira");
-  print('Fernando Augusto');
-
+  print("Amanda A. Mendanha");
   return 6 * 7;
 }
