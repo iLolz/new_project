60ed081fe3c6962e16074ca850bba31be5d2d33d
leandro-mandu <leandro.om15@gmail.com>
2022-09-08 22:05:58 -0300

    add name leandro

--- lib/new_project.dart
+++ lib/new_project.dart
@@ -1,7 +1,15 @@
 int calculate() {
+  print("Alessandro Bento da Silva");
   print("Alencar A. Moreira");
+  print("Paula F. Rodrigues");
+  print("Vanessa de Oliveira Mello");
   print("Fabio Martinelli");
   print("Hugo Pedro Daudt Junior");
+  print("Mateus C. Rossi");
+  print("Evandro Pedro Alves de Mendonça.");
+  print("Rodrigo A. OLiveira");
+  print("Fábio Wood");
   print("Leandro Oliveira");
+
   return 6 * 7;
 }
