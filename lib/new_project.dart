3246f41a65b66fe24a4400b9e07c9400e38c459a
Hugo Pedro Daudt Junior <daudthugo@gmail.com>
2022-09-08 22:08:11 -0300

    hugo commit

--- lib/new_project.dart
+++ lib/new_project.dart
@@ -2,7 +2,7 @@ int calculate() {
   print("Alencar A. Moreira");
   print("Vanessa de Oliveira Mello");
   print("Fabio Martinelli");
-  print("Hugo Pedro Daudt Junior");
+  print("Hugo Pedro Daudt Jr");
   print("Evandro Pedro Alves de Mendonça.");
   print("Rodrigo A. OLiveira");
   return 6 * 7;
