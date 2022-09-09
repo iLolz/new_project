41471525e7b22adbf218979fae4934b48211074d
Alencar <alencarantonio@hotmail.com.br>
2022-09-08 21:14:26 -0300

    init project

---
+++ bin/new_project.dart
@@ -0,0 +1,5 @@
+import 'package:new_project/new_project.dart' as new_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${new_project.calculate()}!');
+}
---
+++ test/new_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:new_project/new_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
